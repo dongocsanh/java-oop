0f10c45efcf11e0fcc92c738e7c0ec44e76f8bde
Admin <Admin@D20923>
2015-05-23 14:32:17 +0700

    asdfa

---
+++ Example01/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rectangle recta = new Rectangle();
+		Rectangle rectb = new Rectangle();
+		recta.setValues(3, 4);
+		rectb.setValues(5, 6);
+		System.out.println("rect a:"+ recta.area());
+		System.out.println("rect b:"+ rectb.area());
+	}
+
+}
